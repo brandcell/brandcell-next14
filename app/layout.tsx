339c61bfaa2864a75b21ec7cf2112013a0d4a605
brian <briansh1225@gmail.com>
2024-07-22 00:10:25 +0800

    added ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon_io/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicon_io/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicon_io/favicon-16x16.png"
+        />
+        <link rel="manifest" href="favicon_io/site.webmanifest"></link>
+      </head>
       <body className={`${manrope.className}`}>
         <Navbar />
         <div id="padding-provider" className="m-auto max-w-screen-xl px-3">
