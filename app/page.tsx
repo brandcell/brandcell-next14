88c43fcad821e1d7f66a63abf66ed9f3947469c4
brian <briansh1225@gmail.com>
2024-08-12 12:39:41 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,8 @@ export default function Home() {
           Marketing?
         </h2>
 
+        <Link href={"/"}>Bugs Test</Link>
+
         <div id="frustration-left" className="xl:max-w-[50%]">
           <div className="frustration-grid grid place-items-center gap-4 xl:grid-cols-2">
             {questions.map((question) => (
